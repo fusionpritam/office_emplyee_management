1b2f0f78ed939a2bfcb46c14d15a7e0a06a6f421
Pritam Bhattacharjee <pritambh2000@gmail.com>
2022-08-03 01:44:30 +0530

    first_commit

--- off_emp_proj/settings.py
+++ off_emp_proj/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d9u)w&jdxqeb6b*wo)x@&)#ihtvxt-a7ume*itimk6akd7ar$)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','office-employee-management.herokuapp.com/']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
